9c1d7ad0faf26d4583050d3ea8de4ee3cc2176a0
David Emanuel Muresan <emanuel.muresan@emmanuel.ro>
2023-10-15 15:28:33 +0300

    tema 1 git

---
+++ Tema1git/Tema1git/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Impartire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+
+            x = Convert.ToInt32(Console.ReadLine("x="));
+            y = Convert.ToInt32(Console.ReadLine("y="));
+            z = x / y;
+
+            if (y != 0 && x % y == 0)
+            {
+                Console.WriteLine(z);
+            }
+            else
+            {
+                Console.WriteLine("Impartirea nu poate fi efectuata");
+            }
+        }
+    }
+}
